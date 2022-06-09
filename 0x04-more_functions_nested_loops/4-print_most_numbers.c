99818be83949fc6abb852bf447058b3877075958
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-09 14:30:27 -0700

    commit for print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int j;
 
-	for (j = 0; j < 10; i++)
+	for (j = 0; j < 10; j++)
 	{
 		if (j != 2 && j != 4)
 		{
-			_putchar(i);
+			_putchar(j + '0');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
