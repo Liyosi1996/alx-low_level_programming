d71a51b46e03ca955d378277bc1b696b1a87b76a
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-09 15:03:54 -0700

    function that draws a straight line in the terminal commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Straight line to the terminal
+ * @m: character in use
+ * Return: 0
+*/
+
+void print_line(int m)
+{
+	if (m <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int j;
+
+
+		for (j = 0; j <= m; j++)
+		{
+			_putchar('_')
+		}
+	}
+	_putchar('\n');
+}
 
