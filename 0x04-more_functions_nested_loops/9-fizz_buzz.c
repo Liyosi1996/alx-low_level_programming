ee5d5d5ac0d40e4cd1024c1259c7366b8688eea2
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-09 16:25:57 -0700

    commit for FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,29 +16,24 @@ int main(void)
 	int k;
 
 	for (k = 1; k <= 100; k++)
-
 	{
-
-		if (k % 15 == 0)
-
-			printf("FizzBuzz");
-
-		else if (k % 3 ==  0)
-
+		if (k % 3 == 0 && k %  5 != 0)
+		{
 			printf("Fizz");
-
-		else if (k % 5 == 0)
-
+		} else if (k % 5 == 0 && k % 3 != 0)
+		{
 			printf("Buzz");
-		else
-			printf("%k", k);
-
-		if (k < 100)
-
-			printf(" ");
-
+		} else if (k % 3 == 0 && k % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (k == 1)
+		{
+			printf("%d", k);
+		} else
+		{
+			print (" %d", k);
+		}
 	}
-
 	printf("\n");
 	return (0);
 }
