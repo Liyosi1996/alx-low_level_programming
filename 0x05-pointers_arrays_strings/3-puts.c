229b1cabb0aacc65f99c63deebd926cdeb77e3a4
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-13 11:11:09 -0700

    commit for function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: pointer to the string to print
+*/
+
+void _puts(char *str)
+{
+	int j;
+
+	j = 0;
+
+	while (str[j] != '\0', j++)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
