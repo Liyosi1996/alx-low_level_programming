2fd25cd167b2bbcb072a7bc464af76e9b2056f03
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-13 06:50:58 -0700

    commit for the README.md

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+include "main.h"
+
+/**
+ * swap_int - swaps thevalues of two integers
+ * @a: pointer to the first value
+ * @b: pointer the second value
+*/
+
+void swap_int(int *a, int *b)
+{
+	int k;
+
+	k = 
