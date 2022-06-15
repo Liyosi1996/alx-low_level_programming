d65d385f3a558bb053246e173aff488ee74a2014
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-15 16:05:41 -0700

    commit for 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,8 +5,7 @@
  * cap_string - capitalizes all words of a string
  * @s: string whose wprds are to be capitalized
  * Description:
- * Separators of words: space, tabulation, new line, ,, ;, ., !,
- * ?, ", (, ), {, and }
+ * Separators of words: space, tabulation, new line
  * Return: 0
 */
 
