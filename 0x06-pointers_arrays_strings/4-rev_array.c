381736eef959001bcf7b30e8fb08027dfcaf8e4e
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-15 13:44:40 -0700

    reverse of strings commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an
+ * array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, b, temp;
+
+	b  = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[b];
+		a[b--] = temp;
+	}
+}
