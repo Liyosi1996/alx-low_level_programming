1e6858b5a4fdaefff8c1ea57c0d60bdf3c11c929
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-16 04:31:34 -0700

    encoding using rot13 commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: to be encoded
+ * Return: 0
+ *
+*/
+
+char *rot13(char *s)
+{
+	char *a = "ÄBCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
