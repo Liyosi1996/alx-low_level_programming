cfc1c376619c6c56b813e878551b61fe67a9c4c2
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-16 06:58:31 -0700

    commit for 6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,8 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: string whose wprds are to be capitalized
- * Description:
- * Separators of words: space, tabulation, new line
+ * cap_string - capitalizes words ina string
+ * @s: string to be modified
  * Return: 0
 */
 
@@ -13,19 +11,16 @@ char *cap_string(char *s)
 {
 	int a, b;
 
-	char len[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
-		'(', ')', '{', '}'};
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (a = 0; s[a] != '\0'; a++)
-
 	{
 		if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
-
 			s[a] -= 32;
-
 		for (b = 0; b < 13; b++)
 		{
-			if (s[a] == len[b])
+			if (s[a] == spe[b])
 			{
 				if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
 				{
@@ -34,6 +29,5 @@ char *cap_string(char *s)
 			}
 		}
 	}
-
 	return (s);
 }
