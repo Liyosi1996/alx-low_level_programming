14786f689af87ef9fee639d09ac44f2c07987ae0
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-22 11:30:19 -0700

    commit for2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: length to be calculated
+ * Return: 0
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
