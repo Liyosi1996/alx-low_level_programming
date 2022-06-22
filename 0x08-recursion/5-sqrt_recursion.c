9339417ff4ec2141a2e6dce1c03dc30b95324031
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-22 14:00:28 -0700

    commit for prime numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns sqrt of a number
+ * @n: number
+ * Return: 0
+*/
+
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+int actual_prime(int n, int j);
+
+/**
+ * is_prime_number - returns integer as a prime number
+ * @n: number to evaluate
+ * Return:0
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates if a number is prime recursive
+ * @n: number
+ * @j: iterator
+ * Return: 0
+*/
+
+int actual_prime(int n, int j)
+{
+	if (j == 1)
+		return (1);
+	if (n % j == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, j - 1));
+}
