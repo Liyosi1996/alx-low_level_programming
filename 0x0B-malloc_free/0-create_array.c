002fe53cfaa41c1ecba70cbd11a34864ecf9f8e6
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-27 17:56:27 +0300

    commit for create array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int b;
+	unsigned int b = 0;
 	char *ptr;
+
+	if (size == 0)
+		return (NULL);
 
 	ptr = (char *) malloc(size * sizeof(char));
 	if (ptr == NULL)
 
-		return (NULL);
+		return (0);
 
 	for (b = 0; b < size; b++)
 	{
