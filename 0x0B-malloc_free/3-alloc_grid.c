ac28326d2bcdb0172e80d9ff1079ec9137a08064
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-27 23:08:00 +0300

    commit for 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <Stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - creates 2D array
+ * @width: width of grid
+ * @height: height of grid
+ * Return:0
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int a, b;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	ptr = (int **) malloc(height * sizeof(int *));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (a = 0; i < height; a++)
+	{
+		ptr[a] = (int *) malloc(width * sizeof(int));
+		if (ptr[a] == NULL)
+		{
+			free(ptr);
+			for (b = 0; b <= a; b++)
+				free(ptr[b]);
+			return (NULL);
+		}
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+		{
+			ptr[a][b] = 0;
+		}
+	}
+	return (ptr);
+}
