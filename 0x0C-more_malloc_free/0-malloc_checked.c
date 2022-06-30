5ed7efc714a851d3b0de2d98fa827263193d6fbe
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-30 16:51:00 +0300

    0-malloc_checked.c commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,47 +1,20 @@
-	        double *d;
-				
-				    c = malloc_checked(sizeof(char) * 1024);
-				        printf("%p\n", (void *)c);
-					    i = malloc_checked(sizeof(int) * 402);
-					        printf("%p\n", (void *)i);
-						    f = malloc_checked(sizeof(float) * 100000000);
-						        printf("%p\n", (void *)f);
-							    d = malloc_checked(INT_MAX);
-							        printf("%p\n", (void *)d);
-								    free(c);
-								        free(i);
-									    free(f);
-									        free(d);
-										    return (0);
-	}#include "main.h"
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <limits.h>
-	
-	/**
-	 *  * main - check the code
-	 *   *
-	 *    * Return: Always 0.
-	 *     */
-	int main(void)
-	{
-		    char *c;
-		        int *i;
-			    float *f;
-			        double *d;
-				
-				    c = malloc_checked(sizeof(char) * 1024);
-				        printf("%p\n", (void *)c);
-					    i = malloc_checked(sizeof(int) * 402);
-					        printf("%p\n", (void *)i);
-						    f = malloc_checked(sizeof(float) * 100000000);
-						        printf("%p\n", (void *)f);
-							    d = malloc_checked(INT_MAX);
-							        printf("%p\n", (void *)d);
-								    free(c);
-								        free(i);
-									    free(f);
-									        free(d);
-										    return (0);
-	});
+#include <stdio.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: bytes to be allocated
+ *
+ * Return: pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
