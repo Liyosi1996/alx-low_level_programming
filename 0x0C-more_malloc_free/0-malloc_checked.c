c9512196295ea5d3cab6e7fbfdda6dfc2e283c24
Liyosi1996 <liyosifaith99@gmail.com>
2022-06-30 13:56:00 +0300

    çommit for 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stddio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: unsigned int type
+ * Return: pointer
+ *
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b)
+		if (ptr == NULL)
+			exit(98);
+	return (ptr);
+}
