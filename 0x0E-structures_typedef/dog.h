188703d42276b086aed7bdc2e6cb65edf96854f7
Liyosi1996 <liyosifaith99@gmail.com>
2022-07-04 22:52:15 +0300

    dog.h commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_h
+#define DOG_H
+
+/**
+ * struct dog - define a new strucyt dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * @name: name of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
