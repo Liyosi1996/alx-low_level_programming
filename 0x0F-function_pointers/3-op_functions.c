8bd29306ebd922dde32f4745574e7de8a3d8c1fe
Liyosi1996 <liyosifaith99@gmail.com>
2022-07-07 00:01:26 +0300

    op_function commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction of two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: product
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: division
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: modulo
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
