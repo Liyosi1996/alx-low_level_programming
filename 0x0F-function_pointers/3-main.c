6b9b081c618ed2f7655fdbc21d9326680cbf7287
Liyosi1996 <liyosifaith99@gmail.com>
2022-07-06 20:57:36 +0300

    3-calc.h commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
