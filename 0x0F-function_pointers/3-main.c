77eae5b34778a59612460e4535f4c18fb5e66a3e
Liyosi1996 <liyosifaith99@gmail.com>
2022-07-06 21:12:26 +0300

    3-main.c commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,3 +10,36 @@
 */
 
 int main(int argc, char *argv[])
+{
+	int a, b, result;
+	char *p;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(a, b);
+	printf("%d\n", result);
+	return (0);
+}
