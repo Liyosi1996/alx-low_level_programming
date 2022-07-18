0ed612aa14fb26ae56078ce6cc9c7e81773c0b7b
Liyosi1996 <liyosifaith99@gmail.com>
2022-07-18 21:33:22 +0300

    list.h header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,18 +22,18 @@ listint_t *find_listint_loop(listint_t *head);
 
 
 /**
- * struct list_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: int
  * @next: pointer to the next node
  *
  * Description: singly linked list for alx project
 */
 
-typedef struct list_s
+typedef struct listint_s
 {
 	int n;
-	struct list_s *next;
-} list_t;
+	struct listint_s *next;
+} listint_t;
 
 
 #endif
