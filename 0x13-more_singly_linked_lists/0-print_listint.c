1cd596ce774bef84caa2835553ad93f332cbd018
Liyosi1996 <liyosifaith99@gmail.com>
2022-07-18 21:38:04 +0300

    function that prints all the element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t node = 0;
 
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
+		node++;
+
 		h = h->next;
+
 	}
-	return (nodes);
+	return (node);
 }
