4acc4a9af81d25630aabd7bdfddbaa74552e6448
Faith Liyosi <78802200+Liyosi1996@users.noreply.github.com>
2022-09-01 23:32:01 +0300

    8-delete_dnodeint.c
    delete file commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,40 +1,63 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes a node
- * @head: double pointer to the linked list
- * @index: index at which to delete node
- * Return: index at which to delete node
-*/
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
 
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+/**
+ * delete_dnodeint_at_index - deltes a node in a doubly linked list
+ * at a given index
+ * @head: double pointer to the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int j;
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
 
-	if (head == NULL|| *head == NULL)
+	if (*head == NULL || dlistint_len(temp) < index + 1)
 		return (-1);
-	current = *head;
-	if (index == 0)
+
+	if (!index)
 	{
-		*head = current->next;
-		if (current->next != NULL)
-		{
-			currrent->next->prev = NULL;
-		}
-		free(current);
+		(*head) = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+		temp->next = NULL;
+		free(temp);
 		return (1);
 	}
-	for (j = 0; j < index; j++)
+
+	while (i < index)
 	{
-		if (current->next == NULL)
-			return (-1);
-		current = current->next;
+		temp = temp->next;
+		i++;
 	}
-	current->prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-	free(current);
+
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	free(temp);
+
 	return (1);
 }
