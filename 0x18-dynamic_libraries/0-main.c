a766a138fb66b91c6c158e846de46bc413270517
Liyosi1996 <liyosifaith99@gmail.com>
2022-09-05 20:51:34 +0300

    main.c file

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * Return: 0 always
+*/
+
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
