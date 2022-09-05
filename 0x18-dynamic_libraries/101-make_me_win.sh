19f6324156ed5ee184f49640966e8a2ddedb5367
Liyosi1996 <liyosifaith99@gmail.com>
2022-09-05 23:42:41 +0300

    make me wish file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-wget -P /tmp/win.so https://github.com/Liyosi1996/alx-low_level_programming/blob/master/0x18-dynamic_libraries/wins.so
-export LD_PRELOAD=/tmp/wins.so
 
+wget -P /tmp https://github.com/Liyosi1996/alx-low_level_programming
+/raw/master/0x18-dynamic_libraries/libmask.so
+
+export LD_PRELOAD="$PWD/../libgiga.so"
